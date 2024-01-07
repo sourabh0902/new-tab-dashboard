8ccfb2e51ea7e67592bf2f14deec1631e6a06226
sourabh0902 <sg456190@gmail.com>
2024-01-07 12:56:57 +0530

    Features: 1. 24 hrs clock 2. Date 3. Greetings on time based with user name 4. Random Quote 5. Dynamic Background with ssr

--- components/Clock.js
+++ components/Clock.js
@@ -1,36 +1,50 @@
-"use client";
 import React, { useEffect, useState } from "react";
-import styles from  "../styles/Clock.module.css";
+import styles from "../styles/Clock.module.css";
 
-const Clock = ({clockHours, clockMinutes}) => {
+const Clock = ({ clockHours, clockMinutes }) => {
   //Saving hours and minutes outside the useeffect
   const [getHours, setgetHours] = useState(null);
   const [getMinutes, setgetMinutes] = useState(null);
+
+  //Saving Date outside the useeffect
+  const [getDate, setgetDate] = useState(null);
 
   useEffect(() => {
     function updateTime() {
       const windowTime = new Date();
 
+      const year = windowTime.getFullYear();
+      const month = (windowTime.getMonth() + 1).toString().padStart(1);
+      const day = windowTime.getDate().toString().padStart(1);
+
+      const formattedDate = `${day}/${month}/${year}`;
+      setgetDate(formattedDate);
+
       const hours = windowTime.getHours();
-      const minutes = windowTime.getMinutes().toString().padStart(2, '0');;
+      const minutes = windowTime.getMinutes().toString().padStart(2, "0");
 
       setgetHours(hours);
       setgetMinutes(minutes);
 
-      clockHours(hours)
-      clockMinutes(minutes)
+      clockHours(hours);
+      clockMinutes(minutes);
     }
 
     updateTime();
 
-    const timeInterval = setInterval(updateTime, 6000);
+    const timeInterval = setInterval(updateTime, 30000);
 
     return () => clearInterval(timeInterval);
   }, []);
 
   return (
     <div className={styles.ClockCss}>
-      {getHours}:{getMinutes}
+      <div className={styles.DateCss}>
+        {getDate}
+      </div>
+      <div className={styles.TimeCss}>
+        {getHours}:{getMinutes}
+      </div>
     </div>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,33 @@
-import Background from "@/components/Background.js";
+import Background from "@/components/Background";
 import React from "react";
 
-const page = () => {
-  //unsplash API key
-  const unsplash_key = process.env.NEXT_PUBLIC_UNSPLASH_API_KEY;
+export async function getServerSideProps() {
+  // Fetching data from two different sources
+  const [imagesRes] = await Promise.all([
+    fetch(
+      `https://api.unsplash.com/search/photos?&query=Scenery&page=1&per_page=1&orientation=landscape&client_id=${process.env.NEXT_PUBLIC_UNSPLASH_API_KEY}`
+    ),
+  ]);
 
-  //Base URl for image
-  const API_URL = "https://api.unsplash.com/search/photos";
+  // Parse the JSON from both responses
+  const [imagesData] = await Promise.all([imagesRes.json()]);
+  console.log(imagesData)
 
+  // Now you have both usersData and imagesData available to be passed as props
+  return {
+    props: {
+      imagesData, // This will be available as this.props.imagesData inside the page component
+    },
+  };
+}
+
+// Make sure to accept the props in your page component
+const Page = ({ imagesData }) => {
   return (
     <div>
-      <Background unsplash_key={unsplash_key} API_URL={API_URL} />
+      <Background imageData={imagesData} />
     </div>
   );
 };
 
-export default page;
+export default Page;
